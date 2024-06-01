656c608cb666114b7b79d0df684da7504c8485f8
dineshkota3 <dineshkota3@gmail.com>
2024-06-01 15:19:26 +0200

    Added greece

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -12,6 +12,22 @@ const Nairobi1 = require('./nairobi/nairobi1.jpg');
 const Nairobi2 = require('./nairobi/nairobi2.jpg');
 const Nairobi3 = require('./nairobi/nairobi3.jpg');
 const Nairobi4 = require('./nairobi/nairobi4.jpg');
+const Aegina1 = require('./aegina/aegina1.jpg');
+const Aegina2 = require('./aegina/aegina2.jpg');
+const Aegina3 = require('./aegina/aegina3.jpg');
+const Zakynthos1 = require('./zakynthos/zakynthos1.jpg');
+const Zakynthos2 = require('./zakynthos/zakynthos2.jpg');
+const Zakynthos3 = require('./zakynthos/zakynthos3.jpg');
+const Zakynthos4 = require('./zakynthos/zakynthos4.jpg');
+const Zakynthos5 = require('./zakynthos/zakynthos5.jpg');
+const Poros1 = require('./poros/poros1.jpg');
+const Poros2 = require('./poros/poros2.jpg');
+const Poros3 = require('./poros/poros3.jpg');
+const Poros4 = require('./poros/poros4.jpg');
+const Athens1 = require('./athens/athens1.jpg');
+const Athens2 = require('./athens/athens2.jpg');
+const Athens3 = require('./athens/athens3.jpg');
+const Athens4 = require('./athens/athens4.jpg');
 
 export default {
     Dog: Dog,
@@ -27,5 +43,21 @@ export default {
     Nairobi1: Nairobi1,
     Nairobi2: Nairobi2,
     Nairobi3: Nairobi3,
-    Nairobi4: Nairobi4
+    Nairobi4: Nairobi4,
+    Aegina1: Aegina1,
+    Aegina2: Aegina2,
+    Aegina3: Aegina3,
+    Zakynthos1: Zakynthos1,
+    Zakynthos2: Zakynthos2,
+    Zakynthos3: Zakynthos3,
+    Zakynthos4: Zakynthos4,
+    Zakynthos5: Zakynthos5,
+    Poros1: Poros1,
+    Poros2: Poros2,
+    Poros3: Poros3,
+    Poros4: Poros4,
+    Athens1: Athens1,
+    Athens2: Athens2,
+    Athens3: Athens3,
+    Athens4: Athens4
     };
