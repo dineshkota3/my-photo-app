5dead0161651c58fca347e5ffb42cc8324851ecf
dineshkota3 <dineshkota3@gmail.com>
2024-06-02 20:10:31 +0200

    Added new locations

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -28,6 +28,54 @@ const Athens1 = require('./athens/athens1.jpg');
 const Athens2 = require('./athens/athens2.jpg');
 const Athens3 = require('./athens/athens3.jpg');
 const Athens4 = require('./athens/athens4.jpg');
+const Masaimara1 = require('./masaimara/masaimara1.jpg');
+const Masaimara2 = require('./masaimara/masaimara2.jpg');
+const Masaimara3 = require('./masaimara/masaimara3.jpg');
+const Masaimara4 = require('./masaimara/masaimara4.jpg');
+const Masaimara5 = require('./masaimara/masaimara5.jpg');
+const Mombasa1 = require('./mombasa/mombasa1.jpg');
+const Mombasa2 = require('./mombasa/mombasa2.jpg');
+const Mombasa3 = require('./mombasa/mombasa3.jpg');
+const Qatar1 = require('./qatar/qatar1.jpg');
+const Qatar2 = require('./qatar/qatar2.jpg');
+const Qatar3 = require('./qatar/qatar3.jpg');
+const Qatar4 = require('./qatar/qatar4.jpg');
+const Zurich1 = require('./zurich/zurich1.jpeg');
+const Zurich2 = require('./zurich/zurich2.jpeg');
+const Zurich3 = require('./zurich/zurich3.jpeg');
+const Zurich4 = require('./zurich/zurich4.jpeg');
+const Lucerne1 = require('./lucerne/lucerne1.jpg');
+const Lucerne2 = require('./lucerne/lucerne2.jpg');
+const Lucerne3 = require('./lucerne/lucerne3.jpg');
+const Interlaken1 = require('./interlaken/interlaken1.jpg');
+const Interlaken2 = require('./interlaken/interlaken2.jpg');
+const Interlaken3 = require('./interlaken/interlaken3.jpg');
+const Interlaken4 = require('./interlaken/interlaken4.jpg');
+const Milan1 = require('./milan/milan1.jpeg');
+const Milan2 = require('./milan/milan2.jpeg');
+const Milan3 = require('./milan/milan3.jpeg');
+const Venice1 = require('./venice/venice1.jpeg');
+const Venice2 = require('./venice/venice2.jpeg');
+const Venice3 = require('./venice/venice3.jpeg');
+const Venice4 = require('./venice/venice4.jpeg');
+const Venice5 = require('./venice/venice5.jpeg');
+const CinqueTerre1 = require('./cinqueterre/cinqueterre1.jpeg');
+const CinqueTerre2 = require('./cinqueterre/cinqueterre2.jpeg');
+const CinqueTerre3 = require('./cinqueterre/cinqueterre3.jpeg');
+const Rome1 = require('./rome/rome1.jpeg');
+const Rome2 = require('./rome/rome2.jpeg');
+const Rome3 = require('./rome/rome3.jpeg');
+const Rome4 = require('./rome/rome4.jpeg');
+const Rome5 = require('./rome/rome5.jpeg');
+const Capri1 = require('./capri/capri1.jpeg');
+const Capri2 = require('./capri/capri2.jpeg');
+const Capri3 = require('./capri/capri3.jpeg');
+const Capri4 = require('./capri/capri4.jpeg');
+const Paris1 = require('./paris/paris1.jpeg');
+const Paris2 = require('./paris/paris2.jpeg');
+const Paris3 = require('./paris/paris3.jpeg');
+const Paris4 = require('./paris/paris4.jpeg');
+const Paris5 = require('./paris/paris5.jpeg');
 
 export default {
     Dog: Dog,
@@ -59,5 +107,53 @@ export default {
     Athens1: Athens1,
     Athens2: Athens2,
     Athens3: Athens3,
-    Athens4: Athens4
+    Athens4: Athens4,
+    Masaimara1: Masaimara1,
+    Masaimara2: Masaimara2,
+    Masaimara3: Masaimara3,
+    Masaimara4: Masaimara4,
+    Masaimara5: Masaimara5,
+    Mombasa1: Mombasa1,
+    Mombasa2: Mombasa2,
+    Mombasa3: Mombasa3,
+    Qatar1: Qatar1,
+    Qatar2: Qatar2,
+    Qatar3: Qatar3,
+    Qatar4: Qatar4,
+    Zurich1: Zurich1,
+    Zurich2: Zurich2,
+    Zurich3: Zurich3,
+    Zurich4: Zurich4,
+    Lucerne1: Lucerne1,
+    Lucerne2: Lucerne2,
+    Lucerne3: Lucerne3,
+    Interlaken1: Interlaken1,
+    Interlaken2: Interlaken2,
+    Interlaken3: Interlaken3,
+    Interlaken4: Interlaken4,
+    Milan1: Milan1,
+    Milan2: Milan2,
+    Milan3: Milan3,
+    Venice1: Venice1,
+    Venice2: Venice2,
+    Venice3: Venice3,
+    Venice4: Venice4,
+    Venice5: Venice5,
+    CinqueTerre1: CinqueTerre1,
+    CinqueTerre2: CinqueTerre2,
+    CinqueTerre3: CinqueTerre3,
+    Rome1: Rome1,
+    Rome2: Rome2,
+    Rome3: Rome3,
+    Rome4: Rome4,
+    Rome5: Rome5,
+    Capri1: Capri1,
+    Capri2: Capri2,
+    Capri3: Capri3,
+    Capri4: Capri4,
+    Paris1: Paris1,
+    Paris2: Paris2,
+    Paris3: Paris3,
+    Paris4: Paris4,
+    Paris5: Paris5
     };
